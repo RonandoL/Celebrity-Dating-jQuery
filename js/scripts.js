f9f480aedbfda1379b46aeff6853cf73e802cda0
Ronando Long <ronando.long@gmail.com>
2016-03-14 13:41:26 -0700

    Initial commit. Exercise completed.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var age = $('#age').val();
+    var genderPref = $('#genderPref').val();
+
+    $('.men-sixty').hide();
+    $('.men-fifty').hide();
+    $('.men-under50').hide();
+    $('.woman-sixty').hide();
+    $('.woman-fifty').hide();
+    $('.woman-under50').hide();
+
+    if (age >= 60 && genderPref === 'men') {
+      $('.men-sixty').show();
+    } else if (age >= 50 && age < 60 && genderPref === 'men') {
+      $('.men-fifty').show();
+    } else if (age < 50 && genderPref === 'men') {
+      $('.men-under50').show();
+    } else if (age >= 60 && genderPref === 'women') {
+      $('.woman-sixty').show();
+    } else if (age >= 50 && age < 60 && genderPref === 'women') {
+      $('.woman-fifty').show();
+    } else if (age < 50 && genderPref === 'women') {
+      $('.woman-under50').show();
+    } else {
+
+    }
+  });
+
+});
